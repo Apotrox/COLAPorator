999a1aa60a4c9b42b101efc76b0088ae5d86e3df
Apotrox <apotrox@gmail.com>
2025-08-21 22:06:51 +0200

    Added string representations for Topic and Category Class

--- src/categories/Category.py
+++ src/categories/Category.py
@@ -5,3 +5,9 @@ class Category:
         self.title = title
         self.angle_begin = angle_begin
         self.angle_end = angle_end
+        
+    def __str__(self):
+        return f"{self.id}, {self.title}, {self.angle_begin}, {self.angle_end}"
+
+    def __repr__(self):
+        return f"{self.id}, {self.title}, {self.angle_begin}, {self.angle_end}"
--- src/topics/Topic.py
+++ src/topics/Topic.py
@@ -3,3 +3,10 @@ class Topic:
         self.id = id
         self.title = title
         self.description = description
+        
+    
+    def __str__(self):
+        return f"{self.id}, {self.title}, {self.description}"
+
+    def __repr__(self):
+        return f"{self.id}, {self.title}, {self.description}"
