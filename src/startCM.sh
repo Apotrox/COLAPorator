3e4f12576aba460b2045fcff71a57230eabfc447
Apotrox <apotrox@gmail.com>
2025-09-23 15:54:35 +0200

    added executables for each tool

---
+++ src/startCM.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/home/admin/Desktop/Bachelor_Thesis/src/.venv/bin/python /home/admin/Desktop/Bachelor_Thesis/src/content_manager.py
---
+++ src/startSetup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/home/admin/Desktop/Bachelor_Thesis/src/.venv/bin/python -m tools.config_new
