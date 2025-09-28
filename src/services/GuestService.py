62f5bb940bd07a8b28eed36774615c81cd730b59
Apotrox <apotrox@gmail.com>
2025-09-28 18:21:15 +0200

    Adding more functionality to GuestService

--- src/services/GuestService.py
+++ src/services/GuestService.py
@@ -3,10 +3,33 @@ from database.database_manager import Manager
 from dataclasses import astuple
 from datetime import datetime
 
+from typing import List
+
+
 class GuestService:
     def __init__(self, db:Manager):
         self.db = db
     
+    def list(self) -> List[Guest]:
+        query = self.db.execute("SELECT * FROM guests").fetchall()
+        return sorted([Guest(id,name,inst,role,pov) for (id,name,inst,role,pov,date) in query], key=lambda x:x.id)
+    
+    def get_for_id(self, id: int) -> Guest|None:
+        query = self.db.execute("SELECT * FROM guests WHERE id=?", (id,)).fetchone()
+        if query:
+            id,name,inst,role,pov, _ = query
+            return Guest(id,name,inst,role,pov)
+        else:
+            return None
+    
     def add_entry(self, guest:Guest):
-        self.db.execute("INSERT INTO guests (name, institution,role,purpose_of_visit, date) VALUES (?,?,?,?,?)",(astuple(guest)+(datetime.now(),)))
+        _,name,institution,role,pov = guest
+        self.db.execute("INSERT INTO guests (name, institution,role,purpose_of_visit, date) VALUES (?,?,?,?,?)",(name,institution,role,pov, datetime.now()))
+        self.db.commit_changes()
+        
+    def delete_entry(self, guest_id: int | Guest):
+        if isinstance(guest_id, Guest):
+            guest_id = guest_id.id
+        
+        self.db.execute("DELETE FROM guests WHERE id=?",(guest_id,))
         self.db.commit_changes()
